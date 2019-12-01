872d6e8a16ec9e34b90b41117793f890a9a243c6
Zixuan Liu <liuzx1121@hotmail.com>
2019-12-01 02:02:43 -0800

    Adding data prepare scripts

--- data/prepare.js
+++ data/prepare.js
@@ -1,13 +1,12 @@
 const PNG = require('pngjs').PNG;
 const fs = require('fs');
-
-const data = JSON.parse(fs.readFileSync('tmp.json'));
+const jsondata = JSON.parse(fs.readFileSync('tmp.json'));
 const name = process.argv[2];
-const u = data.u;
-const v = data.v;
+const u = jsondata.u;
+const v = jsondata.v;
 
-const width = u.Ni;
-const height = u.Nj - 1;
+const width = jsondata.width
+const height = jsondata.height - 1
 
 const png = new PNG({
     colorType: 2,
@@ -19,14 +18,26 @@ const png = new PNG({
 for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
         const i = (y * width + x) * 4;
-        const k = y * width + (x + width / 2) % width;
-        png.data[i + 0] = Math.floor(255 * (u.values[k] - u.minimum) / (u.maximum - u.minimum));
-        png.data[i + 1] = Math.floor(255 * (v.values[k] - v.minimum) / (v.maximum - v.minimum));
+        const k = y * width + Math.floor(x) % width;
+        if (u.values[k] < u.specs.minimum || u.values[k] > u.specs.maximum) {
+            png.data[i + 0] = 0;
+        }
+        else {
+            png.data[i + 0] = Math.floor(255 * (u.values[k] - u.specs.minimum) / (u.specs.maximum - u.specs.minimum));
+            //console.log(u.specs.maximum);
+            //console.log(Math.floor(255 * (u.values[k] - u.specs.minimum) / (u.specs.maximum - u.specs.minimum)));
+        }
+        if (v.values[k] < v.specs.minimum || v.values[k] > v.specs.maximum) {
+            png.data[i + 1] = 0;
+        }
+        else {
+            png.data[i + 1] = Math.floor(255 * (v.values[k] - v.specs.minimum) / (v.specs.maximum - v.specs.minimum));
+        }
         png.data[i + 2] = 0;
         png.data[i + 3] = 255;
     }
 }
-
+console.log(name)
 png.pack().pipe(fs.createWriteStream(name + '.png'));
 
 fs.writeFileSync(name + '.json', JSON.stringify({
@@ -34,10 +45,10 @@ fs.writeFileSync(name + '.json', JSON.stringify({
     date: formatDate(u.dataDate + '', u.dataTime),
     width: width,
     height: height,
-    uMin: u.minimum,
-    uMax: u.maximum,
-    vMin: v.minimum,
-    vMax: v.maximum
+    uMin: u.specs.minimum,
+    uMax: u.specs.maximum,
+    vMin: v.specs.minimum,
+    vMax: v.specs.maximum
 }, null, 2) + '\n');
 
 function formatDate(date, time) {
